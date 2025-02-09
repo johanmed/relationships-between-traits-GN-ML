cb53c03f68b1054dc002427954e933904d32e656
Johannes Medagbe <johanmedagbe@gmail.com>
2025-02-09 05:55:39 +0300

    Clusters and distances found by classical unsupervised learning added to dataset

--- scripts/generalize_cluster_assignment.py
+++ scripts/generalize_cluster_assignment.py
@@ -13,9 +13,9 @@ import joblib
 
 import os
 
-# 1. Read in original data again as full_desc was dropped
+# 1. Read in original data again
 
-full_X_rec=pd.read_csv('../../../diabetes_gemma_association_data_plrt_filtered.csv', usecols=['chr_num', 'pos', 'p_lrt', 'full_desc'])
+full_X_rec=pd.read_csv('../../../diabetes_gemma_association_data_plrt_filtered.csv', usecols=['chr_num', 'pos', 'p_lrt'])
 
 #print('full_X_rec looks like: \n', full_X_rec)
 
@@ -41,26 +41,30 @@ def predict_cluster_distance(full_X_rec, preprocessing_type, model):
 
 best_model=input('Please type the abbreviation of the best model you have got: ') # Get best model information from keyboard for dynamism
 
-if os.path.exists(f'birch_clustering/{best_model}_clustering_hits.pkl'):
+if os.path.exists(f'{best_model}_clustering/{best_model}_clustering_hits.pkl'):
 
     best_model_clust=joblib.load('birch_clustering/birch_clustering_hits.pkl')
     
-    clusters_hits, distances_hits=predict_cluster(full_X_rec, preprocessing_hits, best_model_clust)
+    clusters_hits, distances_hits=predict_cluster_distance(full_X_rec, preprocessing_hits, best_model_clust)
     
     full_X_rec['clusters_hits']=clusters_hits
     full_X_rec['distances_hits']=distances_hits
 
 
-if os.path.exists(f'birch_clustering/{best_model}_clustering_qtl.pkl'):
+if os.path.exists(f'{best_model}_clustering/{best_model}_clustering_qtl.pkl'):
     
     best_model_clust=joblib.load('birch_clustering/birch_clustering_qtl.pkl')
     
-    clusters_qtl, distances_qtl=predict_cluster(full_X_rec, preprocessing_hits, best_model_clust)
+    clusters_qtl, distances_qtl=predict_cluster_distance(full_X_rec, preprocessing_qtl, best_model_clust)
     
     full_X_rec['clusters_qtl']=clusters_qtl
     full_X_rec['distances_qtl']=distances_qtl
 
 
-#print('Extended full_X_rec looks like:\n', full_X_rec)
+desc_X=pd.read_csv('../../../diabetes_gemma_association_data_plrt_filtered.csv', usecols=['full_desc'])
 
-full_X_rec.to_csv('../../../diabetes_gemma_association_data_plrt_filtered_clustering_data.csv', index=False)
+to_save=pd.concat([full_X_rec, desc_X], axis=1)
+
+#print('to save looks like:\n', to_save)
+
+to_save.to_csv('../../../diabetes_gemma_association_data_plrt_filtered_clustering_data.csv', index=False)
